40c868137dba831910da8e0731470e2560e628fb
Eduardo Parra <helloeduardo@icloud.com>
2020-10-16 09:47:52 -0600

    Add average age of students to professor show page

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -12,5 +12,15 @@ describe "As a visitor" do
       expect(page).to have_content(harry.name)
       expect(page).to have_content(longbottom.name)
     end
+
+    it 'I see a average student age a professor has' do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      harry = snape.students.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      longbottom = snape.students.create(name: "Neville Longbottom" , age: 12 , house: "Gryffindor" )
+
+      visit "/professors/#{snape.id}"
+
+      expect(page).to have_content('Average Age: 11.5')
+    end
   end
 end
--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+  describe 'instance methods' do
+    it '#average_age_of_students' do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      harry = snape.students.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      longbottom = snape.students.create(name: "Neville Longbottom" , age: 12 , house: "Gryffindor" )
+
+      expect(snape.average_age_of_students).to eq(11.5)
+    end
+  end
 end
