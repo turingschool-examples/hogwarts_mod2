274f2b14226b436a54c8770b0e2f0df99904d710
James Belta <james.belta@gmail.com>
2020-10-16 11:14:50 -0600

    story 4

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -3,4 +3,10 @@ has_many :professor_students
 has_many :students, through: :professor_students
 
 validates_presence_of :name, :age, :specialty
+
+  def average_student_age
+    no_of_stu = self.students.count
+    age_sum = self.students.sum(:age)
+    age_sum.to_f / no_of_stu.to_f
+  end
 end
--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -2,23 +2,39 @@ require "rails_helper"
 
 describe "As a visitor" do
   describe "When I visit '/professors/:id"do
-  before :each do
-    @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
-    @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
-    @longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
-    ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
-    ProfessorStudent.create(student_id: @longbottom.id, professor_id: @snape.id)
+    before :each do
+      @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      @hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      @lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
 
-    @lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
-    @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
-    ProfessorStudent.create(student_id: @malfoy.id, professor_id: @lupin.id)
-  end
+      @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      @longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
+      ProfessorStudent.create(student_id: @harry.id, professor_id: @hagarid.id)
+      ProfessorStudent.create(student_id: @harry.id, professor_id: @lupin.id)
+      ProfessorStudent.create(student_id: @malfoy.id, professor_id: @hagarid.id)
+      ProfessorStudent.create(student_id: @malfoy.id, professor_id: @lupin.id)
+      ProfessorStudent.create(student_id: @longbottom.id, professor_id: @snape.id)
+    end
     it "I see a list of of the name of students the professors have" do
+      binding.pry
       visit "/professors/#{@snape.id}"
 
       expect(page).to have_content(@harry.name)
       expect(page).to have_content(@longbottom.name)
       expect(page).to_not have_content(@malfoy.name)
+    end
+    it "I see the average age of all students for that professor" do
+      visit "/professors/#{@snape.id}"
+      expect(page).to have_content(11)
+
+      visit "/professors/#{@hagarid.id}"
+      expect(page).to have_content(11.5)
+
+      visit "/professors/#{@lupin.id}"
+      expect(page).to have_content(11.5)
     end
   end
 end
