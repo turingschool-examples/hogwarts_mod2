4efff580d987748217439841137f9f105f68b6db
Kate Tester <katemorris@gmail.com>
2020-10-16 10:23:47 -0600

    Modifies user story test to include number of professors

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -6,16 +6,25 @@ describe 'As a visitor' do
       @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
       @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
       @longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      ProfessorStudent.create(student_id: @harry.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: @harry.id, professor_id: hagrid.id)
+      ProfessorStudent.create(student_id: @harry.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: @malfoy.id, professor_id: hagrid.id)
+      ProfessorStudent.create(student_id: @malfoy.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: @longbottom.id, professor_id: snape.id)
     end
 
     it "I see a list of students with information" do
       visit '/students'
 
-      expect(page).to have_content(@longbottom.name)
-      expect(page).to have_content(@longbottom.age)
-      expect(page).to have_content(@longbottom.house)
-      expect(page).to have_content(@harry.name)
-      expect(page).to have_content(@malfoy.age)
+      expect(page).to have_content("#{@longbottom.name}: 1")
+      expect(page).to have_content("#{@harry.name}: 2")
+      expect(page).to have_content("#{@malfoy.name}: 3")
     end
 
     it "I see the list in alphabetical order" do
