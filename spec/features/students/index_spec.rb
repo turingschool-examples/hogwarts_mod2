0e1c0d4c8614d46c01ebcb3eef5a4f44aa54fb76
Eugene Theriault <hybridbassist@gmail.com>
2020-10-16 09:34:51 -0700

    Create test to check for order of students

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -10,14 +10,13 @@ describe 'As a visitor' do
                                     age: 50,
                                     specialty: "Potions")
 
-      @student1 = @professor1.students.create!(name: "Harry Potter",
-                                              age: "11",
-                                              house: "Gryffindor")
-
-      @student2 = @professor1.students.create!(name: "Hermione Granger",
+      @student1 = @professor1.students.create!(name: "Hermione Granger",
                                               age: "12",
                                               house: "Gryffindor")
 
+      @student2 = @professor1.students.create!(name: "Harry Potter",
+                                              age: "11",
+                                              house: "Gryffindor")
       @professor2.students << @student2
   end
 
@@ -27,6 +26,15 @@ describe 'As a visitor' do
       
       expect(page).to have_content("#{@student1.name}: 1")
       expect(page).to have_content("#{@student2.name}: 2")
+    end
+
+    it "lists students alphabetically by name" do
+      visit '/students'
+
+      within('.students-index') do
+        expect(page.all('p')[0]).to have_content(@student2.name)
+        expect(page.all('p')[1]).to have_content(@student1.name)
+      end
     end
   end
 end
