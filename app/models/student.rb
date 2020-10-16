0d1eaa0470deb8f42e8624e82948d5a7a6b992f2
Eduardo Parra <helloeduardo@icloud.com>
2020-10-16 09:26:19 -0600

    Add validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student <ApplicationRecord
-  validates_presence_of :name, :house
+  validates_presence_of :name, :house, :age
   has_many :professor_students
   has_many :professors, through: :professor_students
 end
