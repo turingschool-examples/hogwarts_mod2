10d80915ada871bcabd813aabcdcded3112a9ac8
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-16 10:27:47 -0600

    WIP

--- app/controllers/professor_students_controller.rb
+++ app/controllers/professor_students_controller.rb
@@ -1,7 +1,9 @@
 class ProfessorStudentsController < ApplicationController
   def index
-    require "pry"; binding.pry
+
     student_ids = ProfessorStudent.where("professor_id = #{params[:professor_id]}").pluck(:student_id).to_a
-    Student.where("student_id = #{student_ids}").pluck(:name)
+    @students = student_ids.map do |id|
+      Student.where("id = #{id}").pluck(:name).first
+    end
   end
 end
