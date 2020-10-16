82265c3e089423b000590129575dc1efb3e9d678
Eugene Theriault <hybridbassist@gmail.com>
2020-10-16 09:31:23 -0700

    Create test to check that professors are ordered by name

--- spec/features/professors/index_spec.rb
+++ spec/features/professors/index_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe "As A Visitor" do
   before :each do
-    @professor1 = Professor.create(name: "Minerva McGonagall",
-                                   age: 204,
-                                   specialty: "Transfiguration")
-
-    @professor2 = Professor.create(name: "Severus Snape",
+    @professor1 = Professor.create(name: "Severus Snape",
                                    age: 50,
                                    specialty: "Potions")
+
+    @professor2 = Professor.create(name: "Minerva McGonagall",
+                                   age: 204,
+                                   specialty: "Transfiguration")
   end
 
   describe "when I visit /professors" do
@@ -22,6 +22,15 @@ describe "As A Visitor" do
       expect(page).to have_content(@professor2.name)
       expect(page).to have_content(@professor2.age)
       expect(page).to have_content(@professor2.specialty)
+    end
+
+    it "shows professors alphabetically by name" do
+      visit '/professors'
+      
+      within '.professor-index' do
+        expect(page.all('p')[0]).to have_content(@professor2.name)
+        expect(page.all('p')[1]).to have_content(@professor1.name)
+      end
     end
   end
 end
