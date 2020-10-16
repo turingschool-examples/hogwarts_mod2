14c74acf2bbc2f114557e7366db115f2a68fab94
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-16 12:11:47 -0500

    Add test for professors method in Student model

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  describe 'class methods' do
+    let!(:professor1) { create(:professor) } 
+    let!(:professor2) { create(:professor, name: 'Taylor Phillips') } 
+    let!(:professor3) { create(:professor, name: 'Mike Jones') } 
+
+    let!(:student1) { create(:student) } 
+    let!(:student2) { create(:student, name: "Ron Weasley") } 
+    let!(:student3) { create(:student, name: "Luna Lovegood") } 
+
+    let!(:professor_student1) { create(:professor_student, professor_id: professor1.id, student_id: student1.id) } 
+    let!(:professor_student2) { create(:professor_student, professor_id: professor1.id, student_id: student2.id) } 
+    let!(:professor_student3) { create(:professor_student, professor_id: professor2.id, student_id: student3.id) } 
+    let!(:professor_student4) { create(:professor_student, professor_id: professor3.id, student_id: student1.id) } 
+
+    it '.professors' do
+      expect(student1.professors).to eq([professor1, professor3])
+    end
+  end
 end
