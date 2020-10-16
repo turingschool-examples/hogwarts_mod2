0a79309ac2a60232a4eb480cb8ad8019fcaf386d
Eduardo Parra <helloeduardo@icloud.com>
2020-10-16 09:58:28 -0600

    Add students index with professor count

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student <ApplicationRecord
   validates_presence_of :name, :house, :age
   has_many :professor_students
   has_many :professors, through: :professor_students
+
+  def professor_count
+    professors.count
+  end
 end
--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -1,30 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe Student, type: :model do
-
-  describe 'validations' do
-    it {should validate_presence_of :name}
-    it {should validate_presence_of :age}
-    it {should validate_presence_of :house}
-  end
-
-  describe 'relationships' do
-    it {should have_many :professor_students}
-    it {should have_many(:professors).through(:professor_students)}
-  end
-
-  describe 'instance methods' do
-    it '#professor_count' do
+describe "As a visitor" do
+  describe 'When I visit /students' do
+    it 'I see a list of students and their number of professors' do
       snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
       hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
 
       malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
 
       ProfessorStudent.create(student_id: malfoy.id, professor_id: hagarid.id)
       ProfessorStudent.create(student_id: malfoy.id, professor_id: snape.id)
 
-      expect(malfoy.professor_count).to eq(2)
+      ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+
+      visit '/students'
+
+      expect(page).to have_content("#{malfoy.name}: 2")
+      expect(page).to have_content("#{longbottom.name}: 1")
     end
   end
-
 end
