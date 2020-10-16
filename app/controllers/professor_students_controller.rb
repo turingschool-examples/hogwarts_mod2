fc4088eaa563579265e0a52217920c9838b042db
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-16 10:31:43 -0600

    Add lists students on professor_students index

--- app/controllers/professor_students_controller.rb
+++ app/controllers/professor_students_controller.rb
@@ -1,9 +1,5 @@
 class ProfessorStudentsController < ApplicationController
   def index
-
-    student_ids = ProfessorStudent.where("professor_id = #{params[:professor_id]}").pluck(:student_id).to_a
-    @students = student_ids.map do |id|
-      Student.where("id = #{id}").pluck(:name).first
-    end
+    @student_ids = ProfessorStudent.where("professor_id = #{params[:professor_id]}").pluck(:student_id).to_a
   end
 end
