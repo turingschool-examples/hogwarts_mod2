056f2553bb2d1c978ff8d4ebf6ee142354a51351
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-16 10:04:49 -0600

    Add test and method for average student age

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -16,4 +16,16 @@ RSpec.describe "As a visitor" do
       expect(page).to have_content(longbottom.name)
     end
   end
+  it "I see the average age of all students for that professor." do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+      visit "/professors/#{snape.id}"
+
+      expect(page).to have_content("Average Age: #{snape.student_average_age}")
+  end
 end
