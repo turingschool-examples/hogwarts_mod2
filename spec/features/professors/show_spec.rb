727898db20e7039a491636ee52d91b1b2b629994
Kiera Allen <kieraaallen@gmail.com>
2020-10-16 10:07:17 -0600

    Finish User Story 2

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -17,6 +17,10 @@ RSpec.describe "As a visitor" do
       malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
       longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
 
+      ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
       visit "/professors/#{snape.id}"
 
       expect(page).to have_content("#{harry.name}")
