a8fa3b0a67a478b6a887d5ab07ca8d1fcd9ba3cb
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-16 10:17:19 -0600

    Create spec for average age on professors show page

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -30,3 +30,32 @@ describe 'as a visitor' do
     end
   end
 end
+
+describe 'as a visitor' do
+  describe 'when I visit a professors show page'do
+    it 'has the average age of all students for that professor' do
+      minerva = Professor.create!({ name: "Minerva McGonagall",
+                                    age: 204,
+                                    specialty: "Transfiguration"})
+      harry   = Student.create!({   name: "Harry Potter",
+                                    age: 16,
+                                    house: "Gryffindor"})
+      luna    = Student.create!({   name: "Luna Lovegood",
+                                    age: 15,
+                                    house: "Ravenclaw"})
+      draco   = Student.create!({   name: "Draco Malfoy",
+                                    age: 16,
+                                    house: "Slytherin"})
+      ps_1   = ProfessorStudent.create!({ professor_id: minerva.id,
+                                          student_id:   harry.id})
+      ps_2   = ProfessorStudent.create!({ professor_id: minerva.id,
+                                          student_id:   luna.id})
+      ps_3   = ProfessorStudent.create!({ professor_id: minerva.id,
+                                          student_id:   draco.id})
+
+      visit "/professors/#{minerva.id}"
+
+      expect(page).to have_content("Average Age: #{Student.average(:age).round}")
+    end
+  end
+end
