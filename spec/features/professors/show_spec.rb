ba311ed95915f2c2e3d38a58aace3d5d370e15a4
Curtis Bartell <cbartell980@gmail.com>
2020-10-16 10:42:34 -0600

    Create show_spec.rb

---
+++ spec/features/professors/show_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+# User Story 2 of 4
+# As a visitor,
+# When I visit '/professors/:id'
+# I see a list of the names of the students the professors have.
+# (e.g. "Neville Longbottom"
+#      "Hermione Granger"
+#      "Luna Lovegood")
