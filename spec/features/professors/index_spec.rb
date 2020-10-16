27d07546fe84e21632b4efd5c7b56a5a37154ba5
Curtis Bartell <cbartell980@gmail.com>
2020-10-16 10:22:45 -0600

    Add test for professor info on professor index page

--- spec/features/professors/index_spec.rb
+++ spec/features/professors/index_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
-# User Story 1 of 4
-# As a visitor,
-# When I visit '/professors',
-# I see a list of professors with the following information:
-# -Name
-# -Age
-# -Specialty
-# (e.g. "Name: Minerva McGonagall, Age: 204, Specialty: Transfiguration")
+RSpec.describe "Professors Features:" do
+  describe "As a visitor" do
+    describe "When I visit '/professors'" do
+      it "I see a list of professors with their name, age, and specialty" do
+        snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+        hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+        lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+        professors = [snape, hagrid, lupin]
+
+        visit '/professors'
+
+        professors.each do |professor|
+          within("professor-#{professor.id}") do
+            expect(page).to have_content("Name: #{professor.name}")
+            expect(page).to have_content("Age: #{professor.age}")
+            expect(page).to have_content("Specialty: #{professor.specialty}")
+          end
+        end
+      end
+    end
+  end
+end
