7dbf08964ee15c18d9110f3e099ac27663724b44
Kate Tester <katemorris@gmail.com>
2020-10-16 10:27:17 -0600

    Adds test for num_professors method

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,6 +13,29 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  describe 'instance methods' do
+    it "num_professors" do
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: hagrid.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: hagrid.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+      expect(harry.num_professors).to eq(3)
+      expect(longbottom.num_professors).to eq(1)
+      expect(malfoy.num_professors).to_not eq(3)
+    end
+  end
+
   describe 'class methods' do
     before(:each) do
       @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
