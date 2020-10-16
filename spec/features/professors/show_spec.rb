be5443856a3a10f2d08d28966a44f9b6c2cdf06b
Kiera Allen <kieraaallen@gmail.com>
2020-10-16 09:48:17 -0600

    Create test for User Story 2

---
+++ spec/features/professors/show_spec.rb
@@ -0,0 +1,27 @@
+# User Story 2 of 4
+# As a visitor,
+# When I visit '/professors/:id'
+# I see a list of the names of the students the professors have.
+# (e.g. "Neville Longbottom"
+#      "Hermione Granger"
+#      "Luna Lovegood")
+
+require 'rails_helper'
+
+RSpec.describe "As a visitor" do
+  describe "when I visit the '/professors/:id' path" do
+    it "can see a list of student names taught by that professor" do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      visit "/professors/#{snape.id}"
+
+      expect(page).to have_content("#{harry.name}")
+      expect(page).to have_content("#{malfoy.name}")
+      expect(page).to have_content("#{longbottom.name}")
+    end
+  end
+end
