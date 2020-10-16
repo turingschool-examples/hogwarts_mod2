f6ca6463aae175b5b83e5eeae17545e90369b26e
Brett Turing <bjsherman80@gmail.com>
2020-10-16 10:37:57 -0600

    created test for average student age method

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+    describe 'instance method' do
+    it "can count average age of professors students" do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      snape.students << harry
+      snape.students << longbottom
+
+     expect(snape.average_student_age).to eq(11)
+   end
+  end
 end
