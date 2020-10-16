d5819b40e514d9eb3b65de2a8a385cadf6007069
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-10-16 10:53:32 -0600

    Create test for professor show page to display average age of all students for professor

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -34,5 +34,19 @@ describe "as a visitor" do
       expect(page).to have_content(@harry.name)
       expect(page).to have_content(@malfoy.name)
     end
+    it "then I see the average age of all students for that professor" do
+      visit("/professors/#{@snape.id}")
+
+      expect(page).to have_content("Average Age: 11")
+
+      visit("/professors/#{@hagarid.id}")
+
+      expect(page).to have_content("Average Age: 11.5")
+
+      visit("/professors/#{@lupin.id}")
+
+      expect(page).to have_content("Average Age: 11.5")
+
+    end
   end
 end
