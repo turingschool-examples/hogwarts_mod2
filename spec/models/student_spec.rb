85b66202b3d751efb3d6cfc1fc67a85d4dbb3842
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-16 11:33:29 -0600

    Add testing for count_of_professors method in student model

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,27 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  describe 'count_of_professors' do
+    it "can find how many professors a student has" do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: hagarid.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: hagarid.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+      expect(harry.count_of_professors).to eq(3)
+      expect(malfoy.count_of_professors).to eq(2)
+      expect(longbottom.count_of_professors).to eq(1)
+    end
+  end
+
 end
