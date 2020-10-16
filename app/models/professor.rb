8af9f86d17b0141e02898697846a568883a77abf
Brett Turing <bjsherman80@gmail.com>
2020-10-16 10:38:46 -0600

    create average student age

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -2,4 +2,8 @@ class Professor <ApplicationRecord
   has_many :professor_students
   has_many :students, through: :professor_students
   validates_presence_of :name, :age, :specialty
+
+  def average_student_age
+    (students.pluck(:age).sum / students.count).round(1).to_f
+  end
 end
