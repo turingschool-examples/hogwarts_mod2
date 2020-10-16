584dd6c380dbff246a91b18992439ddf2492cf05
Joshua Carey <jdcarey128@gmail.com>
2020-10-16 10:37:10 -0600

    Add student index with professor count

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   get '/professors', to: 'professors#index'
   get '/professors/:professor_id', to: 'professors#show'
-
+  get '/students', to: 'students#index'
 end
