7fc52d91e9ddf472ae4110a8135911077819b5c1
Nick King <nickmaxking@gmail.com>
2020-10-16 10:14:04 -0600

    Add test for display of average student age

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -44,3 +44,46 @@ describe "As a visitor," do
     end
   end
 end
+
+describe "As a visitor," do
+  describe "When I visit '/professors/:id'" do
+    it "I see the average age of all students for that professor" do
+      professor_1 = Professor.create(
+        name: "Minerva McGonagall",
+        age: 204,
+        specialty: "Transfiguration"
+      )
+      student_1 = Student.create(
+        name: "Harry Potter",
+        age: 16,
+        house: "Gryffindor"
+      )
+      student_2 = Student.create(
+        name: "Draco Malfoy",
+        age: 16,
+        house: "Slytherin"
+      )
+      student_3 = Student.create(
+        name: "Luna Lovegood",
+        age: 15,
+        house: "Ravenclaw"
+      )
+      ProfessorStudent.create(
+        professor: professor_1,
+        student: student_1
+      )
+      ProfessorStudent.create(
+        professor: professor_1,
+        student: student_2
+      )
+      ProfessorStudent.create(
+        professor: professor_1,
+        student: student_3
+      )
+
+      visit "/professors/#{professor_1.id}"
+
+      expect(page).to have_content("Average Age: 15.7")
+    end
+  end
+end
