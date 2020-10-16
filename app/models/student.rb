71434d0d536497440f8744afd990ca91fa2f58b3
Austin Aspaas <araspaas@live.com>
2020-10-16 11:26:31 -0600

    Add count of professors and sort by name method

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,13 @@ class Student <ApplicationRecord
                         :age,
                         :house
   has_many :professor_students
-  has_many :professors, through: :professor_students                      
+  has_many :professors, through: :professor_students
+
+ def count_of_professors
+    professors.count
+  end
+
+  def self.sort_by_name
+   order(:name)
+ end
 end
