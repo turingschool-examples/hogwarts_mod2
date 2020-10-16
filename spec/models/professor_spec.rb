71665571287c79288cab262f9b7846ab8dfb2e96
Kiera Allen <kieraaallen@gmail.com>
2020-10-16 10:45:39 -0600

    Create instance method test

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+  describe 'instance methods' do
+    it '.average_student_age' do
+      hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+
+      ProfessorStudent.create(student_id: harry.id, professor_id: hagarid.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: hagarid.id)
+
+      expect(hagarid.average_student_age).to eq(11.5)
+    end
+  end
 end
