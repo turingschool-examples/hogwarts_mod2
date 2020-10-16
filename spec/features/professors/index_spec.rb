b447f0bd35eb36f914c448b7ab48399f5eec1d4e
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-16 10:15:09 -0600

    Add test for ordered prof names

--- spec/features/professors/index_spec.rb
+++ spec/features/professors/index_spec.rb
@@ -11,5 +11,15 @@ RSpec.describe "As a visitor" do
       expect(page).to have_content(snape.age)
       expect(page).to have_content(snape.specialty)
     end
+    it "all information is listed alphabetically" do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+      
+      visit "/professors"
+      expect(page).to have_content(lupin.name)
+      expect(page).to have_content(hagarid.name)
+      expect(page).to have_content(snape.name)
+    end
   end
 end
