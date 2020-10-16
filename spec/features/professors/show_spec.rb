9bd263179a7b7504b45e0eeeb6fd2df92281972c
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-16 10:49:14 -0600

    Add edgetest to prof_show_spec to test it isnt showing all student names

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -5,9 +5,13 @@ describe "As a visitor" do
     it "lists that professor's students' names" do
       snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
       harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
       longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
       ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
       ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
 
       visit("/professors")
 
@@ -17,6 +21,8 @@ describe "As a visitor" do
       expect(page).to have_content("#{snape.name}'s Students")
       expect(page).to have_content("#{harry.name}")
       expect(page).to have_content("#{longbottom.name}")
+      expect(page).to_not have_content("#{malfoy.name}")
+      expect(page).to_not have_content("#{lupin.name}")
     end
   end
 end
