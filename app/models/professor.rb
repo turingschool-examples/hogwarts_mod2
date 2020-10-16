a64cd08e2d5e899473e1cfb09910eb73776fb269
Grant Dempsey <CryptoDemps@gmail.com>
2020-10-16 10:49:14 -0600

    Added avg_student_age model method

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -5,4 +5,8 @@ class Professor <ApplicationRecord
 
   has_many :professor_students
   has_many :students, through: :professor_students
+
+  def avg_student_age
+    Student.average(:age).to_f.round(2)
+  end
 end
