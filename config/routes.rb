f43a743804072df5782d5d4571df3f83ce6bb3bc
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-16 10:25:33 -0600

    Add route, controller, and view for students index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   #professors
   get '/professors', to: 'professors#index'
   get '/professors/:id', to: 'professors#show'
+  get '/students', to: 'students#index'
 end
