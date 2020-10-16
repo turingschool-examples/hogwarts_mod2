289b3e331bd4184dd3f27cac92bc82ce5c913553
Hanna Davis <hanna.e.davis@gmail.com>
2020-10-16 11:21:00 -0600

    Add model test for average student age

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,26 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  describe 'class methods' do 
+    it 'can find the average age of all the students' do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: hagarid.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: hagarid.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+      expect(snape.students.average_age).to eq(11)
+      expect(hagarid.students.average_age).to eq(11.5)
+      expect(lupin.students.average_age).to eq(11.5)
+    end 
+  end
 end
