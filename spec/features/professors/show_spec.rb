2bb9c1ad17fb012ff2c8ed830cb33ba9e0ed4fb2
Eugene Theriault <hybridbassist@gmail.com>
2020-10-16 08:39:27 -0700

    Create test to check that a professor show page has the names of that professors students

---
+++ spec/features/professors/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  before :each do
+    @professor1 = Professor.create(name: "Minerva McGonagall",
+                                   age: 204,
+                                   specialty: "Transfiguration")
+
+    @student1 = @professor1.students.create!(name: "Harry Potter",
+                                             age: "11",
+                                             house: "Gryffindor")
+    @student2 = @professor1.students.create!(name: "Hermione Granger",
+                                             age: "12",
+                                             house: "Gryffindor")
+  end
+
+  describe 'when I visit a professors show page' do
+    it 'I should see the names of all the students that professor has' do
+      visit "/professors/#{@professor1.id}"
+
+      expect(page).to have_content(@student1.name)
+      expect(page).to have_content(@student2.name)
+    end
+  end
+end
