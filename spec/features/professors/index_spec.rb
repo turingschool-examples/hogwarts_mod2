c7e585d499f29a946280b76598a1229ea281b7e7
Brett Turing <bjsherman80@gmail.com>
2020-10-16 09:47:28 -0600

    created test for professor index with targeted testing using css

---
+++ spec/features/professors/index_spec.rb
@@ -0,0 +1,22 @@
+require'rails_helper'
+
+RSpec.describe 'professors index page', type: :feature do
+  it "can see all professors" do
+    snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+    lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+    visit "/professors"
+
+    within("#professor-#{snape.id}") do
+      expect(page).to have_content(snape.name)
+      expect(page).to have_content(snape.age)
+      expect(page).to have_content(snape.specialty)
+    end
+    within("#professor-#{lupin.id}") do
+      expect(page).to have_content(lupin.name)
+      expect(page).to have_content(lupin.age)
+      expect(page).to have_content(lupin.specialty)
+    end
+  end
+end
