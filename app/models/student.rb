3a8424be3fa7ed4c7563fb8b3a82740060ee8bc9
Jose Lopez <joselopez11394@gmail.com>
2020-10-16 11:39:37 -0400

    fixes all models and got all test passing

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student <ApplicationRecord
-
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :house
+  has_many :professor_students
+  has_many :professors, through: :professor_students
 end
