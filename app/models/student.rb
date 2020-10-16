7b32ac1b32573bbb120deae311724dee8f8a5588
Curtis Bartell <cbartell980@gmail.com>
2020-10-16 10:04:00 -0600

    Add one to many relationship with professor_students

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,7 @@ class Student <ApplicationRecord
   validates_presence_of :name
   validates_presence_of :age
   validates_presence_of :house
+
+  # Relationships
+  has_many :professor_students
 end
