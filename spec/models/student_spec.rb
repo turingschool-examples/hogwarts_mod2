2fde96fa865d05ee95608abd9d8053489a20853d
Will Dunlap <dunlapww@gmail.com>
2020-10-16 10:18:44 -0600

    Create model test for num_of_profs

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,19 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  describe 'instance methods' do
+    it 'can determine a students number of profesors' do
+      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: hagarid.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+
+      expect(malfoy.num_of_profs).to eq(3)
+    end
+  end
+
 end
