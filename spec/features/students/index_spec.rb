a527a8818a7e41a27797b2c1b4b4ce503fa6cf78
Jake Heft <jakeheft@gmail.com>
2020-10-16 11:30:34 -0600

    Add test for alphabetical order

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -70,3 +70,73 @@ describe "As a visitor," do
     end
   end
 end
+
+describe "As a visitor" do
+  it "When I visit '/students', I see all info listed alphabetically" do
+    professor_1 = Professor.create(
+      name: "Severus Snape",
+      age: 45,
+      specialty: "Betrayal"
+    )
+    professor_2 = Professor.create(
+      name: "Helena Handbasket",
+      age: 50,
+      specialty: "Witches Studies"
+    )
+    professor_3 = Professor.create(
+      name: "Albus Dumbledore",
+      age: 136,
+      specialty: "Everything"
+    )
+    student_1 = Student.create(
+      name: "Jake Potter",
+      age: 34,
+      house: "Ravenclaw"
+    )
+    student_2 = Student.create(
+      name: "Tommy Hogwarts",
+      age: 12,
+      house: "Gryffindor"
+    )
+    student_3 = Student.create(
+      name: "Jane Magic",
+      age: 15,
+      house: "Hufflepuff"
+    )
+    prof_stdnt_1 = ProfessorStudent.create(
+      professor_id: professor_1.id,
+      student_id: student_1.id
+    )
+    prof_stdnt_2 = ProfessorStudent.create(
+      professor_id: professor_2.id,
+      student_id: student_1.id
+    )
+    prof_stdnt_3 = ProfessorStudent.create(
+      professor_id: professor_3.id,
+      student_id: student_1.id
+    )
+    prof_stdnt_4 = ProfessorStudent.create(
+      professor_id: professor_1.id,
+      student_id: student_2.id
+    )
+    prof_stdnt_5 = ProfessorStudent.create(
+      professor_id: professor_3.id,
+      student_id: student_2.id
+    )
+    prof_stdnt_6 = ProfessorStudent.create(
+      professor_id: professor_2.id,
+      student_id: student_3.id
+    )
+    prof_stdnt_7 = ProfessorStudent.create(
+      professor_id: professor_1.id,
+      student_id: student_3.id
+    )
+
+    visit "/students"
+
+    expected = "Jake Potter: 3
+Jane Magic: 2
+Tommy Hogwarts: 2"
+    expect(page).to have_content(expected)
+  end
+end
