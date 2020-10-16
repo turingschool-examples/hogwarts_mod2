4c7776bc70222addccaca9b1169058a07791ecb3
Kate Tester <katemorris@gmail.com>
2020-10-16 10:13:15 -0600

    Adds test for alphabetizing a list of professors.

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+  describe 'class methods' do
+    it "alpha" do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      expect(Professor.alpha).to eq([lupin, hagrid, snape])
+    end
+  end
 end
