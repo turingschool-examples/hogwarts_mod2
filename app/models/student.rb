3a82a44ac422372d13de416883bcfe3e37f47e71
George Soderholm <georgesoderholm@gmail.com>
2020-10-16 11:59:52 -0600

    Add method to average the age

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,8 @@ class Student <ApplicationRecord
   validates_presence_of :name
   validates_presence_of :age
   validates_presence_of :house
+
+  def self.average_student_age
+    Student.average(:age)
+  end
 end
