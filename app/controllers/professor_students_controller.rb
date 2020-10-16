7a9eb1e96834a9b23068313355d3a42c9d3bd2ab
Zach Stearns <zach.stearns92@gmail.com>
2020-10-16 11:15:15 -0600

    Remove unnecessary lines

--- app/controllers/professor_students_controller.rb
+++ app/controllers/professor_students_controller.rb
@@ -1,8 +1,5 @@
 class ProfessorStudentsController < ApplicationController
   def index
-    # @professors = Professor.all
     @professor = Professor.find(params[:id])
-    @students = Student.all
-
   end
 end
