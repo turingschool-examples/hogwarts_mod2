1c3c2b9f225146e251851db4847c8bfbc3c2e333
Brett Turing <bjsherman80@gmail.com>
2020-10-16 10:11:09 -0600

    created number of professors method test

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,19 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+    describe 'instance method' do
+    it "can count number of professors" do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+
+      harry.professors << snape
+      harry.professors << lupin
+      harry.professors << hagarid
+
+     expect(harry.number_of_professors).to eq(3)
+   end
+  end
 end
