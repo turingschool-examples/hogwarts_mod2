cd3b15730e23d057dfa0c5e4fc34b1ab8530e3dd
Kate Tester <katemorris@gmail.com>
2020-10-16 10:38:01 -0600

    Adds extra styling and a link to professor show page- for fun

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/professors', to: 'professors#index'
-  get '/professors/:id', to: 'professors#show'
+  get '/professors/:id', to: 'professors#show', as: 'prof_show'
 
   get '/students', to: 'students#index'
 end
