e54e185b35b4912660f691e82635c963406d7d5a
Kevin Cuadros <kev040798@gmail.com>
2020-10-16 11:51:14 -0400

    Finish story 1

--- spec/features/professors/index_spec.rb
+++ spec/features/professors/index_spec.rb
@@ -6,3 +6,25 @@ require 'rails_helper'
 # -Name
 # -Age
 # -Specialty
+
+RSpec.describe 'As a visitor' do
+  describe 'when I visit /professors' do
+    it 'should see a list of professors with their information.' do
+      snape = Professor.create(name: 'Severus Snape', age: 45, specialty: 'Potions')
+      hagarid = Professor.create(name: 'Rubeus Hagrid', age: 38, specialty: 'Care of Magical Creatures')
+      lupin = Professor.create(name: 'Remus Lupin', age: 49, specialty: 'Defense Against The Dark Arts')
+
+      visit '/professors'
+
+      expect(page).to have_content(snape.name)
+      expect(page).to have_content(snape.age)
+      expect(page).to have_content(snape.specialty)
+      expect(page).to have_content(hagarid.name)
+      expect(page).to have_content(hagarid.age)
+      expect(page).to have_content(hagarid.specialty)
+      expect(page).to have_content(lupin.name)
+      expect(page).to have_content(lupin.age)
+      expect(page).to have_content(lupin.specialty)
+    end
+  end
+end
