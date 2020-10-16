4de24271371e9ccf16cb50f3b2648dc7e92059b1
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-16 11:54:29 -0500

    Create view and action for professors show page

---
+++ app/controllers/professors_controller.rb
@@ -0,0 +1,9 @@
+class ProfessorsController < ApplicationController
+  def index
+    @professors = Professor.all
+  end
+
+  def show
+    @professor = Professor.find(params[:id])
+  end
+end
--- app/models/professor.rb
+++ app/models/professor.rb
@@ -5,4 +5,12 @@ class Professor <ApplicationRecord
   validates_presence_of :name
   validates_presence_of :age
   validates_presence_of :specialty
+
+  def students
+    student_ids = ProfessorStudent.where(professor_id: self.id).pluck(:student_id)     
+
+    student_ids.flat_map do |id|
+      Student.where(id: id)
+    end  
+  end
 end
