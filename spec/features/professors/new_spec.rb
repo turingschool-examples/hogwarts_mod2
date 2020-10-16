6c99a6d21c9f033bdcb96faa3456fb9187d30438
Roberto Basulto <roberto.basulto085@gmail.com>
2020-10-16 11:57:09 -0400

    Finished user story 1

---
+++ spec/features/professors/new_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+describe 'As a visitor' do
+  describe 'When i visit /professors' do
+    it 'I see a list of professors with name, age, and specialty' do
+      professor = Professor.create(name: 'Severus Snape', age: '45', specialty: 'Potions')
+
+      visit '/professors'
+save_and_open_page
+      expect(page).to have_content("Name: #{professor.name}")
+      expect(page).to have_content("Age: #{professor.age}")
+      expect(page).to have_content("Specialty: #{professor.specialty}")
+    end
+  end
+end
