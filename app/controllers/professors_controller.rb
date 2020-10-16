1abf76ad4ae10cd7e14a9209abdb96a9c9559c66
Curtis Bartell <cbartell980@gmail.com>
2020-10-16 11:06:32 -0600

    Add student names to show page

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -2,4 +2,20 @@ class ProfessorsController < ApplicationController
   def index
     @professors = Professor.all
   end
+
+  def show
+    professor(params[:id])
+    professor_students(@professor.id)
+  end
+
+  private
+
+  def professor(professor_id)
+    @professor ||= Professor.find(professor_id)
+  end
+
+  def professor_students(professor_id)
+    professor(professor_id)
+    @students ||= @professor.students
+  end
 end
