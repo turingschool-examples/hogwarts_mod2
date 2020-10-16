8dad47cc2c864d58f8facc83a34541122d2f1e41
Taylor Phillips <taylorscottphillips@gmail.com>
2020-10-16 12:52:55 -0500

    Rubocop all models and controllers

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,20 +1,20 @@
-class Professor <ApplicationRecord
+class Professor < ApplicationRecord
   has_many :professor_students
-  has_many :students, through: :professor_students 
+  has_many :students, through: :professor_students
 
   validates_presence_of :name
   validates_presence_of :age
   validates_presence_of :specialty
 
   def students
-    student_ids = ProfessorStudent.where(professor_id: self.id).pluck(:student_id)     
+    student_ids = ProfessorStudent.where(professor_id: id).pluck(:student_id)
 
     student_ids.flat_map do |id|
       Student.where(id: id)
-    end  
+    end
   end
 
   def average_student_age
-    ProfessorStudent.joins(:student).where(professor_id: self.id).average(:age).round(1)
+    ProfessorStudent.joins(:student).where(professor_id: id).average(:age).round(1)
   end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,13 +1,13 @@
-class Student <ApplicationRecord
+class Student < ApplicationRecord
   has_many :professor_students
-  has_many :professors, through: :professor_students 
+  has_many :professors, through: :professor_students
 
   validates_presence_of :name
   validates_presence_of :age
   validates_presence_of :house
 
   def professors
-    professor_ids = ProfessorStudent.where(student_id: self.id).pluck(:professor_id)     
+    professor_ids = ProfessorStudent.where(student_id: id).pluck(:professor_id)
 
     professor_ids.flat_map do |id|
       Professor.where(id: id)
