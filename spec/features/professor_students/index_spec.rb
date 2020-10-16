6df62a031980f0bc2921f2b58afd7c05dfcfbeb0
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-16 09:52:26 -0600

    Update test can view students for each professor

--- spec/features/professor_students/index_spec.rb
+++ spec/features/professor_students/index_spec.rb
@@ -6,6 +6,8 @@ describe "As a visitor" do
     (e.g. 'Neville Longbottom'
           'Hermione Granger'
           'Luna Lovegood')" do
+
+
       snape = Professor.create(
                           name: "Severus Snape",
                           age: 45,
@@ -18,7 +20,48 @@ describe "As a visitor" do
                           name: "Remus Lupin",
                           age: 49 ,
                           specialty: "Defense Against The Dark Arts")
-      visit '/professors'
+      harry = Student.create(
+                        name: "Harry Potter" ,
+                        age: 11 ,
+                        house: "Gryffindor" )
+      malfoy = Student.create(
+                        name: "Draco Malfoy" ,
+                        age: 12 ,
+                        house: "Slytherin" )
+      longbottom = Student.create(
+                        name: "Neville Longbottom" ,
+                        age: 11 ,
+                        house: "Gryffindor" )
+      ProfessorStudent.create(
+                          student_id: harry.id,
+                          professor_id: snape.id)
+      ProfessorStudent.create(
+                          student_id: harry.id,
+                          professor_id: hagrid.id)
+      ProfessorStudent.create(
+                          student_id: harry.id,
+                          professor_id: lupin.id)
+      ProfessorStudent.create(
+                          student_id: malfoy.id,
+                          professor_id: hagrid.id)
+      ProfessorStudent.create(
+                          student_id: malfoy.id,
+                          professor_id: lupin.id)
+      ProfessorStudent.create(
+                          student_id: longbottom.id,
+                          professor_id: snape.id)
+
+      visit "/professors/#{snape.id}"
+      expect(page).to have_content("#{harry.name}")
+      expect(page).to have_content("#{longbottom.name}")
+
+      visit "/professors/#{hagrid.id}"
+      expect(page).to have_content("#{harry.name}")
+      expect(page).to have_content("#{malfoy.name}")
+
+      visit "/professors/#{lupin.id}"
+      expect(page).to have_content("#{harry.name}")
+      expect(page).to have_content("#{malfoy.name}")
 
     end
   end
--- spec/features/professors/index_spec.rb
+++ spec/features/professors/index_spec.rb
@@ -15,48 +15,19 @@ describe "As a visitor" do
                           name: "Remus Lupin",
                           age: 49 ,
                           specialty: "Defense Against The Dark Arts")
-      harry = Student.create(
-                        name: "Harry Potter" ,
-                        age: 11 ,
-                        house: "Gryffindor" )
-      malfoy = Student.create(
-                        name: "Draco Malfoy" ,
-                        age: 12 ,
-                        house: "Slytherin" )
-      longbottom = Student.create(
-                        name: "Neville Longbottom" ,
-                        age: 11 ,
-                        house: "Gryffindor" )
-      ProfessorStudent.create(
-                          student_id: harry.id,
-                          professor_id: snape.id)
-      ProfessorStudent.create(
-                          student_id: harry.id,
-                          professor_id: hagrid.id)
-      ProfessorStudent.create(
-                          student_id: harry.id,
-                          professor_id: lupin.id)
-      ProfessorStudent.create(
-                          student_id: malfoy.id,
-                          professor_id: hagrid.id)
-      ProfessorStudent.create(
-                          student_id: malfoy.id,
-                          professor_id: lupin.id)
-      ProfessorStudent.create(
-                          student_id: longbottom.id,
-                          professor_id: snape.id)
+      visit '/professors'
 
-      visit "/professors/#{snape.id}"
-      expect(page).to have_content("#{harry.name}")
-      expect(page).to have_content("#{longbottom.name}")
+      expect(page).to have_content("#{snape.name}")
+      expect(page).to have_content("#{snape.age}")
+      expect(page).to have_content("#{snape.specialty}")
 
-      visit "/professors/#{hagrid.id}"
-      expect(page).to have_content("#{harry.name}")
-      expect(page).to have_content("#{malfoy.name}")
+      expect(page).to have_content("#{hagrid.name}")
+      expect(page).to have_content("#{hagrid.age}")
+      expect(page).to have_content("#{hagrid.specialty}")
 
-      visit "/professors/#{lupin.id}"
-      expect(page).to have_content("#{harry.name}")
-      expect(page).to have_content("#{malfoy.name}")
+      expect(page).to have_content("#{lupin.name}")
+      expect(page).to have_content("#{lupin.age}")
+      expect(page).to have_content("#{lupin.specialty}")
     end
   end
 end
