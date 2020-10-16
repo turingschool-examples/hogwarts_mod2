b77a19c39bbc3774c9667df5ba273981fbc34c6b
Jake Heft <jakeheft@gmail.com>
2020-10-16 10:30:49 -0600

    Add functionality to see students and number of their professors in students index

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   get '/professors', to: 'professors#index'
   get '/professors/:professor_id', to: 'professors#show'
+
+  get '/students', to: 'students#index'
 end
