170f1729e4fc713e9ceddf5a320c0e08cdaa89e2
Austin Aspaas <araspaas@live.com>
2020-10-16 11:26:54 -0600

    Add average student age and sort by name method

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -4,4 +4,12 @@ class Professor <ApplicationRecord
                         :specialty
   has_many :professor_students
   has_many :students, through: :professor_students
+
+  def average_student_age
+    students.average(:age)
+  end
+
+  def self.sort_by_name
+    order(:name)
+  end
 end
