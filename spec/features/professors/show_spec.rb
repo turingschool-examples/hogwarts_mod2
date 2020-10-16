d9fd79a2b163f4793907bfe41c6e57af774fb2cf
Curtis Bartell <cbartell980@gmail.com>
2020-10-16 10:53:10 -0600

    Add test for student names

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -7,3 +7,35 @@ require 'rails_helper'
 # (e.g. "Neville Longbottom"
 #      "Hermione Granger"
 #      "Luna Lovegood")
+
+RSpec.describe "Professors Show:" do
+  describe "As a visitor" do
+    describe "When I visit 'professors/:id'" do
+      it "I see a list of the names of the students that professor has" do
+        snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+        hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+        lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+        professors = [snape, hagrid, lupin]
+        harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+        malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+        longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+        ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+        ProfessorStudent.create(student_id: harry.id, professor_id: hagrid.id)
+        ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+        ProfessorStudent.create(student_id: malfoy.id, professor_id: hagrid.id)
+        ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+        ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+        professors.each do |professor|
+          visit "/professors/#{professor.id}"
+
+          professor.students.each do |student|
+            within("#student-#{student.id}") do
+              student.name
+            end
+          end
+        end
+      end
+    end
+  end
+end
