7c466cb63d7dc72958006a4ce6795dbf4587b023
Grant Dempsey <CryptoDemps@gmail.com>
2020-10-16 09:38:57 -0600

    Added relationships and validations

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,3 +1,8 @@
 class Professor <ApplicationRecord
+  validates_presence_of :name,
+                        :age,
+                        :specialty
 
+  has_many :professor_students
+  has_many :students, through: :professor_students
 end
