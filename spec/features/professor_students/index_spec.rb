9fa18c53a2e04d68206bd5525bbf23589eb394d2
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-10-16 10:57:26 -0600

    Add test average age for students appears on index

--- spec/features/professor_students/index_spec.rb
+++ spec/features/professor_students/index_spec.rb
@@ -60,7 +60,59 @@ describe "As a visitor" do
       visit "/professors/#{lupin.id}"
       expect(page).to have_content("#{harry.name}")
       expect(page).to have_content("#{malfoy.name}")
-
     end
+
+    it "I see the average age of all students for that professor. (e.g. 'Average Age: 14.5')" do
+      snape = Professor.create(
+                          name: "Severus Snape",
+                          age: 45,
+                          specialty: "Potions")
+      hagrid = Professor.create(
+                          name: "Rubeus Hagrid",
+                          age: 38 ,
+                          specialty: "Care of Magical Creatures")
+      lupin = Professor.create(
+                          name: "Remus Lupin",
+                          age: 49 ,
+                          specialty: "Defense Against The Dark Arts")
+      harry = Student.create(
+                        name: "Harry Potter" ,
+                        age: 11 ,
+                        house: "Gryffindor" )
+      malfoy = Student.create(
+                        name: "Draco Malfoy" ,
+                        age: 12 ,
+                        house: "Slytherin" )
+      longbottom = Student.create(
+                        name: "Neville Longbottom" ,
+                        age: 11 ,
+                        house: "Gryffindor" )
+      ProfessorStudent.create(
+                          student_id: harry.id,
+                          professor_id: snape.id)
+      ProfessorStudent.create(
+                          student_id: harry.id,
+                          professor_id: hagrid.id)
+      ProfessorStudent.create(
+                          student_id: harry.id,
+                          professor_id: lupin.id)
+      ProfessorStudent.create(
+                          student_id: malfoy.id,
+                          professor_id: hagrid.id)
+      ProfessorStudent.create(
+                          student_id: malfoy.id,
+                          professor_id: lupin.id)
+      ProfessorStudent.create(
+                          student_id: longbottom.id,
+                          professor_id: snape.id)
+
+      visit "/professors/#{snape.id}"
+      expect(page).to have_content("Average Age: 11")
+
+      visit "/professors/#{hagrid.id}"
+      expect(page).to have_content("Average Age: 11.5")
+
+      visit "/professors/#{lupin.id}"
+      expect(page).to have_content("Average Age: 11.5")
   end
 end
