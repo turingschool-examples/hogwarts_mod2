cda3040afe49c50f109d703f3683b842971c6dd7
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-16 10:26:52 -0600

    Create test for professor model method 'student average age'

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+  describe "methods" do
+    it "should be able to get student_average_age" do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      expect(snape.student_average_age).to eq(nil)
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+      ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+      
+      expect(snape.student_average_age).to eq(11)
+    end
+  end
 end
