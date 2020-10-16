602938bd5576d1e0d86f16c3b25e9dcbccc3285e
Kevin Cuadros <kev040798@gmail.com>
2020-10-16 12:59:28 -0400

    Finish story 4

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -34,5 +34,40 @@ RSpec.describe 'As a visitor' do
       expect(page).to have_content(harry.name)
       expect(page).to have_content(malfoy.name)
     end
+
+    it "should see the average age of all students for that professor" do
+      snape = Professor.create(name: 'Severus Snape', age: 45, specialty: 'Potions')
+      hagarid = Professor.create(name: 'Rubeus Hagrid', age: 38, specialty: 'Care of Magical Creatures')
+      lupin = Professor.create(name: 'Remus Lupin', age: 49, specialty: 'Defense Against The Dark Arts')
+
+      harry = Student.create(name: 'Harry Potter', age: 11, house: 'Gryffindor')
+      malfoy = Student.create(name: 'Draco Malfoy', age: 12, house: 'Slytherin')
+      longbottom = Student.create(name: 'Neville Longbottom', age: 11, house: 'Gryffindor')
+
+      ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: hagarid.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: hagarid.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+      visit "/professors/#{snape.id}"
+
+      within "#average-student-age" do
+        expect(page).to have_content('Average Age: 11')
+      end
+
+      visit "/professors/#{hagarid.id}"
+
+      within "#average-student-age" do
+        expect(page).to have_content('Average Age: 11.5')
+      end
+
+      visit "/professors/#{lupin.id}"
+
+      within "#average-student-age" do
+        expect(page).to have_content('Average Age: 11.5')
+      end
+    end
   end
 end
