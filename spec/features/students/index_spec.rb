83e55031c4dda3026f6751cff206a4a8ffa36e6f
Eugene Theriault <hybridbassist@gmail.com>
2020-10-16 08:48:44 -0700

    Create test to check for students names and number of professors on students index page

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  before :each do
+      @professor1 = Professor.create(name: "Minerva McGonagall",
+                                    age: 204,
+                                    specialty: "Transfiguration")
+
+      @professor2 = Professor.create(name: "Severus Snape",
+                                    age: 50,
+                                    specialty: "Potions")
+
+      @student1 = @professor1.students.create!(name: "Harry Potter",
+                                              age: "11",
+                                              house: "Gryffindor")
+
+      @student2 = @professor1.students.create!(name: "Hermione Granger",
+                                              age: "12",
+                                              house: "Gryffindor")
+
+      @professor2.students << @student2
+  end
+
+  describe 'when I visit /students' do
+    it 'I see a list of students and the number of professors each student has' do
+      visit '/students'
+      
+      expect(page).to have_content("#{@student1.name}: 1")
+      expect(page).to have_content("#{@student2.name}: 2")
+    end
+  end
+end
