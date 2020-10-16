e37800dc64b1d2d47c44ab48dcaac0228dd15c89
Curtis Bartell <cbartell980@gmail.com>
2020-10-16 10:13:57 -0600

    Create professors/index_spec.rb

---
+++ spec/features/professors/index_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+# User Story 1 of 4
+# As a visitor,
+# When I visit '/professors',
+# I see a list of professors with the following information:
+# -Name
+# -Age
+# -Specialty
+# (e.g. "Name: Minerva McGonagall, Age: 204, Specialty: Transfiguration")
