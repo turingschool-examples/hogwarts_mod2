7a55bcaff16b10f2fff86fc09e9fc1f739de72d9
Greg Mitchell <g2mitchell3003@gmail.com>
2020-10-16 09:31:02 -0600

    Set up many to many relationship and add user stories to READ.me

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,3 +1,8 @@
 class Professor <ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :specialty
 
+  has_many :professor_students
+  has_many :students, through: :professor_students
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,8 @@
 class Student <ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :age
+  validates_presence_of :house
 
+  has_many :professor_students
+  has_many :professors, through: :professor_students
 end
