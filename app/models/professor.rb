b36f621593cd04df8032e290c57a81758a5251d2
Hope Gochnour <hope.gochnour@gmail.com>
2020-10-16 11:52:58 -0600

    Add quick commit before the buzzer

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -5,4 +5,9 @@ class Professor <ApplicationRecord
 
   has_many :professor_students
   has_many :students, {through: :professor_students, source: :student}
+
+  def average_student_age
+    count_of_students = ProfessorStudent.where("professor_id == #{id}")
+    students_to_find = ProfessorStudent.select(:student_id).where("professor_id == #{id}")
+  end
 end
