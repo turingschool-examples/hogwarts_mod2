48f4b00c8c09d2045042a72083f257bd6a07f741
Miquel <foymikek@gmail.com>
2020-12-16 14:55:59 -0700

    Add working student test, professor_count

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,6 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
+  before(:each) do
+    @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    @hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+    @lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+    @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+    @longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+    @ps_1 = ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
+    @ps_2 = ProfessorStudent.create(student_id: @harry.id, professor_id: @hagrid.id)
+    @ps_3 = ProfessorStudent.create(student_id: @harry.id, professor_id: @lupin.id)
+    @ps_4 = ProfessorStudent.create(student_id: @malfoy.id, professor_id: @hagrid.id)
+    @ps_5 = ProfessorStudent.create(student_id: @malfoy.id, professor_id: @lupin.id)
+    @ps_6 = ProfessorStudent.create(student_id: @longbottom.id, professor_id: @snape.id)
+  end
 
   describe 'validations' do
     it {should validate_presence_of :name}
@@ -11,6 +27,12 @@ RSpec.describe Student, type: :model do
   describe 'relationships' do
     it {should have_many :professor_students}
     it {should have_many(:professors).through(:professor_students)}
+  end
+
+  describe 'professor_count' do
+    it "outputs total number of professors the student has" do
+      expect(@malfoy.professor_count).to eq(2)
+    end
   end
 
 end
