83cc79dd286c9c2ae89affd5e80a7bf89f050281
Saundra <saundracatalina@gmail.com>
2020-12-16 15:28:07 -0700

    Add class method average_student_age

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,6 +1,10 @@
-class Professor <ApplicationRecord
+class Professor < ApplicationRecord
   validates_presence_of :name, :age, :specialty
 
   has_many :professor_students
   has_many :students, through: :professor_students
+
+  def self.average_student_age
+    Student.average(:age).to_f
+  end
 end
