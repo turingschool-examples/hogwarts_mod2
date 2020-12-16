08472012255ef7498ceb4d931d22598b7bce72f5
Yesi Meza <yesi.meza10@gmail.com>
2020-12-16 15:48:02 -0700

    created student alphabetical test and method

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -12,5 +12,25 @@ RSpec.describe Student, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:professors).through(:professor_students)}
   end
+  describe "methods" do
+      it "can list alphabetically" do 
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: hagrid.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: hagrid.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+      expect(Student.all.alphabetical_students).to eq([malfoy, harry, longbottom])
+    end
+  end 
 
 end
