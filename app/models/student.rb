1cd2e06ab513eeece126101f39b32424a032da28
James Fox-Collis <jlfoxcollis@gmail.com>
2020-12-16 13:24:00 -0700

    3 finished

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,5 +3,7 @@ class Student <ApplicationRecord
   has_many :professors, through: :professor_students
   validates_presence_of :name, :age, :house
 
-
+  def prof_count
+    professors.count
+  end
 end
