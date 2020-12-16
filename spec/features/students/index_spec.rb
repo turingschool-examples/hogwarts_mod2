91bc1e98115a36e0fdf3ad65b95b159267ec8a38
Vanessa Arruda <wanessaarruda@gmail.com>
2020-12-16 14:49:52 -0700

    Add view to pass story 3

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe 'When I visit the students index page' do
+    before :each do
+      @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      @hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      @lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      @longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      ProfessorStudent.create!(
+          professor_id: @snape.id,
+          student_id: @harry.id
+        )
+
+        ProfessorStudent.create!(
+          professor_id: @snape.id,
+          student_id: @longbottom.id
+        )
+      end
+      it 'Shows a list of students and number of professors for each student' do
+        visit '/students'
+        save_and_open_page
+        expect(page).to have_content("#{@harry.name}: 1")
+        expect(page).to have_content("#{@longbottom.name}: 1")
+      end
+  end
+end
