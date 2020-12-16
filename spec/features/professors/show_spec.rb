cc68bd4a7b3f4d9fa964d83c91e8b50928632fc7
Ely Hess <ely.hess@me.com>
2020-12-16 14:15:07 -0700

    add average age test

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -7,8 +7,8 @@ describe 'As a visitor' do
       @hagarid = Professor.create(name: "Rubeus Hagrid", age: 38, specialty: "Care of Magical Creatures")
       @lupin = Professor.create(name: "Remus Lupin", age: 49, specialty: "Defense Against The Dark Arts")
 
-      @harry = Student.create(name: "Harry Potter", age: 11, house: "Gryffindor")
-      @malfoy = Student.create(name: "Draco Malfoy", age: 12, house: "Slytherin")
+      @harry = Student.create(name: "Harry Potter", age: 9, house: "Gryffindor")
+      @malfoy = Student.create(name: "Draco Malfoy", age: 10, house: "Slytherin")
       @longbottom = Student.create(name: "Neville Longbottom", age: 11, house: "Gryffindor")
 
       ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
@@ -26,6 +26,14 @@ describe 'As a visitor' do
 
       expect(page).to have_content(@harry.name)
       expect(page).to have_content(@longbottom.name)
+    end
+
+    it 'I see the average age of all students for that professor.' do
+      visit professor_path(@snape)
+
+      expect(page).to have_content("#{@snape.name}'s Students")
+
+      expect(page).to have_content("Average age: 10")
     end
   end
 end
