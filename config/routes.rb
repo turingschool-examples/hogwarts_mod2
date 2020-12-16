a7a02e3a2fcd6d2ce3ccb5dd7c8839d71dce7663
awosborn <alanturing@example.coclear>
2020-12-16 14:11:32 -0700

    Add story 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   #Professors
   get "/professors", to: "professors#index"
-  get "/professors/:id", to: "professors#index", as: :professors_show
+  get "/professors/:id", to: "professors#show", as: :professors_show
 
   #Students
   get "/students", to: "students#index"
-  get "/students/:id", to: "students#index", as: :students_show
+  get "/students/:id", to: "students#show", as: :students_show
 end
