d128b407ef11cc915f100e48b4a91fff54f3155e
Sam Yeo <yeosamuel95@gmail.com>
2020-12-16 13:35:42 -0700

    Create method full_info to pass test

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -2,4 +2,8 @@ class Professor <ApplicationRecord
   has_many :professor_students
   has_many :students, through: :professor_students
   validates :name, :age, :specialty, presence: true
+
+  def full_info
+    "Name: #{name}, Age: #{age}, Specialty: #{specialty}"
+  end
 end
