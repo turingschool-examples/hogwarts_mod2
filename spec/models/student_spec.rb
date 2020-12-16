50222873f65cc1f14bb0bd00173caa68ba9402fb
awosborn <alanturing@example.coclear>
2020-12-16 14:56:18 -0700

    Update model test, Student

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,12 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  describe 'class methods' do
+    harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+
+
+    Student.average_age.should == 11.5
+  end
+
 end
