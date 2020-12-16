f094b66cff26e524ff515e692491db7f0b5ee20b
Catherine Dean <catherinemdean15@gmail.com>
2020-12-16 13:40:19 -0700

    create professors_controller with index and show actions

---
+++ app/controllers/professors_controller.rb
@@ -0,0 +1,12 @@
+class ProfessorsController < ApplicationController
+
+  def index
+    @professors = Professor.all
+  end
+
+  def show
+    @professor = Professor.find(params[:id])
+  end
+
+
+end
