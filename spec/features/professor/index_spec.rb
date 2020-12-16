49b093ef665eaf0d0da5273b7bf3a380b313bacb
Yesi Meza <yesi.meza10@gmail.com>
2020-12-16 15:47:22 -0700

    created feature test for prof alphabetical

--- spec/features/professor/index_spec.rb
+++ spec/features/professor/index_spec.rb
@@ -15,5 +15,15 @@ RSpec.describe "As a visitor when I visit '/professors'", type: :feature do
     expect(page).to have_content(hagrid.age)
     expect(page).to have_content(hagrid.specialty)
   end
+  it "can see list of professors alphabetically" do 
+    snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+    lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+    visit "/professors"
+
+    expect("#{lupin.name}").to appear_before("#{hagrid.name}")
+    expect("#{hagrid.name}").to appear_before("#{snape.name}")
+  end
 end
 
