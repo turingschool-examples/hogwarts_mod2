d5346e5ab947f3e20f7185c332171099e12351e1
ninesky00 <jiangzho@gmail.com>
2020-12-16 16:23:20 -0600

    add feature average student age with ruby instead of active record

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -2,4 +2,11 @@ class Professor <ApplicationRecord
   has_many :professor_students
   has_many :students, through: :professor_students
   validates :name, :age, :specialty, presence: :true
+
+  def student_average_age
+    total_student_age = students.sum do |student|
+      student.age
+    end
+    total_student_age / students.count
+  end
 end
--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,17 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+  describe 'student average age' do 
+    it "can find average student age" do 
+    snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+    ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+    ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+    average_age = (harry.age + longbottom.age) / 2
+
+    expect(snape.student_average_age).to eq(average_age)
+    end
+  end
 end
