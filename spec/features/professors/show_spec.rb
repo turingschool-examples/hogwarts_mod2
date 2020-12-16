39b3157f982e1bb6b47f347a02d2ffe4a9a25e8e
awosborn <alanturing@example.coclear>
2020-12-16 14:29:20 -0700

    Add test, story 4

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -17,5 +17,18 @@ RSpec.describe 'As a visitor', type: :feature do
       expect(page).to have_content(malfoy.name)
 
     end
+
+    it 'has average age of students' do
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+
+      ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+
+      visit "/professors/#{lupin.id}"
+
+      expect(page).to have_content("11.5")
+    end
   end
 end
