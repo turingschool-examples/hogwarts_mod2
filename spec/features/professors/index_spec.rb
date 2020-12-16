62120bcf54aadad27fc8726508bd8e0d20ea36ed
Grayson Myers <graymyers10@gmail.com>
2020-12-16 13:32:53 -0700

    Add test for professors index page

---
+++ spec/features/professors/index_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+describe "professors index page" do
+  before :each do
+    @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    @hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+    @lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+    visit "/professors"
+  end
+
+  it "has a list of professors with their name, age, and specialty" do
+    expect(page).to have_content(@snape.name)
+    expect(page).to have_content(@snape.age)
+    expect(page).to have_content(@snape.specialty)
+
+    expect(page).to have_content(@hagarid.name)
+    expect(page).to have_content(@hagarid.age)
+    expect(page).to have_content(@hagarid.specialty)
+
+    expect(page).to have_content(@lupin.name)
+    expect(page).to have_content(@lupin.age)
+    expect(page).to have_content(@lupin.specialty)
+  end
+end
