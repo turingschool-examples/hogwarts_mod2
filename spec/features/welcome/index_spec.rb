4e8c1d81f9459ad39c6bcced9ca1dee475ced088
James Fox-Collis <jlfoxcollis@gmail.com>
2020-12-16 15:33:19 -0700

    Added a welcome mat

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "has links" do
+  before :each do
+    @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    @hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+    @lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+    @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+    @longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @hagarid.id)
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @lupin.id)
+    ProfessorStudent.create(student_id: @malfoy.id, professor_id: @hagarid.id)
+    ProfessorStudent.create(student_id: @malfoy.id, professor_id: @lupin.id)
+    ProfessorStudent.create(student_id: @longbottom.id, professor_id: @snape.id)
+  end
+
+  it "can take you places" do
+    visit '/'
+
+    expect(page).to have_content("Professors")
+    expect(page).to have_content("Students")
+    expect(page).to have_content("Welcome!")
+  end
+end
