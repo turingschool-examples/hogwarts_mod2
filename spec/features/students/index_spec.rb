11575d8f66f4d7a20cb003af8798fb0d165456d5
ninesky00 <jiangzho@gmail.com>
2020-12-16 15:30:25 -0600

    add student index test

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "student index" do 
+  before(:each) do 
+    @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    @hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+    @lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+    @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+    @longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @hagarid.id)
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @lupin.id)
+    ProfessorStudent.create(student_id: @malfoy.id, professor_id: @hagarid.id)
+    ProfessorStudent.create(student_id: @malfoy.id, professor_id: @lupin.id)
+    ProfessorStudent.create(student_id: @longbottom.id, professor_id: @snape.id)
+  end 
+
+  it "sees a list of students and the number of professors each student has" do 
+    visit students_path
+
+    expect(page).to have_content(@harry.name: @harry.professors.count)
+    expect(page).to have_content(@malfoy.name: @malfoy.professors.count)
+    expect(page).to have_content(@longbottom.name: @longbottom.professors.count)
+  end
+end
