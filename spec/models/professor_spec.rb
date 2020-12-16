faca01008af65562bac55d0f7267498763cb65cd
Sam Yeo <yeosamuel95@gmail.com>
2020-12-16 13:34:13 -0700

    Add instance variable test to list out full info

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Professor, type: :model do
+  before :each do
+    @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    @hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+  end
   describe 'validations' do
     it {should validate_presence_of :name}
     it {should validate_presence_of :age}
@@ -10,5 +14,11 @@ RSpec.describe Professor, type: :model do
   describe 'relationships' do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
+  end
+  describe 'instance variables' do 
+    it "can list out a professor's full info into one string" do
+      professor_info = "Name: #{@snape.name}, Age: #{@snape.age}, Specialty: #{@snape.specialty}"
+      expect(@snape.full_info).to eq(professor_info)
+    end
   end
 end
