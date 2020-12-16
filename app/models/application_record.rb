188a0b4c9b2f1aae8c86e857c7ea1c56e361fcda
RyanDBarnett <conner2468@gmail.com>
2020-12-16 15:15:06 -0700

    Add extension

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.sort_alphabetically_by_name
+    all.order(name: :asc)
+  end
 end
--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+  describe 'class methods' do
+    describe 'sort_alphabetically' do
+      it 'should return the professors in alphabetical order' do
+        snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+        hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+        lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+        expect(Professor.sort_alphabetically_by_name).to eq([lupin, hagarid, snape])
+      end
+    end
+  end
 end
