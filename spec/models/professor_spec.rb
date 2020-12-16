aa534276aa3c2a9cc7c60d9c4c04222fa6a6fb70
Sam Yeo <yeosamuel95@gmail.com>
2020-12-16 14:13:28 -0700

    Create test for instance method for professor to check for students avg age

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Professor, type: :model do
   before :each do
     @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
-    @hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
   end
   describe 'validations' do
     it {should validate_presence_of :name}
@@ -19,6 +18,13 @@ RSpec.describe Professor, type: :model do
     it "can list out a professor's full info into one string" do
       professor_info = "Name: #{@snape.name}, Age: #{@snape.age}, Specialty: #{@snape.specialty}"
       expect(@snape.full_info).to eq(professor_info)
+    end
+    it 'can get the average age of all students under specific professor' do
+      harry = @snape.students.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      malfoy = @snape.students.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      longbottom = @snape.students.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+      expect(@snape.students_avg_age).to eq((11 + 12 + 11) / 3)
     end
   end
 end
