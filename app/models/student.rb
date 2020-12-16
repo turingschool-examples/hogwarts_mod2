a9cca3d8dcb3ff511c5bf293646fda8fce07d8ec
Isabelle Villasenor <villasenori@outlook.com>
2020-12-16 13:20:07 -0700

    Add passing model tests for all models

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,5 @@
 class Student <ApplicationRecord
   has_many :professor_students, dependent: :destroy
   has_many :professors, through: :professor_students
+  validates_presence_of :name, :age, :house
 end
