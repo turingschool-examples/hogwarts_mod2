d88cc85c34291ac693cae0d2d597ad41b4b964a6
Ely Hess <ely.hess@me.com>
2020-12-16 14:16:20 -0700

    Add alphabetical

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -2,4 +2,9 @@ class Professor <ApplicationRecord
   validates_presence_of :name, :age, :specialty
   has_many :professor_students
   has_many :students, through: :professor_students
+
+  def self.alphabetical
+    order(:name)
+  end
+  
 end
