a1f595e4245bb979ae671b943803b744646c7f02
Grayson Cunningham <gecun289@gmail.com>
2020-12-16 13:36:06 -0700

    add test for student names method on professor class

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Professor, type: :model do
+  before :each do
+    @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    @longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+    @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
+    ProfessorStudent.create(student_id: @longbottom.id, professor_id: @snape.id)
+  end
   describe 'validations' do
     it {should validate_presence_of :name}
     it {should validate_presence_of :age}
@@ -10,5 +19,11 @@ RSpec.describe Professor, type: :model do
   describe 'relationships' do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
+  end
+
+  describe '#student_names' do
+    it 'finds the names of its students' do
+      expect(@snape.student_names).to eq([@harry.name, @longbottom.name])
+    end
   end
 end
