7045d5732e38b7e3d30a77ae66c17a3db7d04a78
Grayson Myers <graymyers10@gmail.com>
2020-12-16 14:10:26 -0700

    Add test for average student age of professor model

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,22 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+  describe "instance methods" do
+    before :each do
+      @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+
+      @harry = Student.create(name: "Harry Potter" , age: 14 , house: "Gryffindor" )
+      @malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      @longbottom = Student.create(name: "Neville Longbottom" , age: 10 , house: "Gryffindor" )
+
+      ProfessorStudent.create(student: @harry, professor: @snape)
+      ProfessorStudent.create(student: @malfoy, professor: @snape)
+      ProfessorStudent.create(student: @longbottom, professor: @snape)
+    end
+
+    it "average_student_age" do
+      expect(@snape.average_student_age).to eq(12)
+    end
+  end
 end
