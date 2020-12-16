fdb6202178157f821edc618f391e7e9e6de77b38
Catherine Dean <catherinemdean15@gmail.com>
2020-12-16 14:54:30 -0700

    order professors and students in index and show

--- app/controllers/professors_controller.rb
+++ app/controllers/professors_controller.rb
@@ -1,11 +1,12 @@
 class ProfessorsController < ApplicationController
 
   def index
-    @professors = Professor.all
+    @professors = Professor.order(:name)
   end
 
   def show
     @professor = Professor.find(params[:id])
+    @students = @professor.students.order(:name)
   end
 
 
