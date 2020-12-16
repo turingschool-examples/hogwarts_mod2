ccb564841b9b2da2ce97c89c88bbe0ba1ab53a3e
Saundra <saundracatalina@gmail.com>
2020-12-16 13:31:48 -0700

    Write test for list of professors and attributes on index page

---
+++ spec/features/professors/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "when I visit '/professors'" do
+  it "can see a list of professors with name, age, speciality" do
+    snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+
+    visit '/professors'
+
+    expect(page).to have_content(snape.name)
+    expect(page).to have_content(snape.age)
+    expect(page).to have_content(snape.specialty)
+    expect(page).to have_content(hagarid.name)
+    expect(page).to have_content(hagarid.age)
+    expect(page).to have_content(hagarid.specialty)
+  end
+end
