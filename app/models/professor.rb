323bf54e907e82421618b4db5394cff06373b8f2
Yesi Meza <yesi.meza10@gmail.com>
2020-12-16 15:05:14 -0700

    created student avg age method and test

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -2,4 +2,8 @@ class Professor < ApplicationRecord
   validates_presence_of :name, :age, :specialty
   has_many :professor_students
   has_many :students, through: :professor_students
+
+  def student_average_age
+    students.average(:age).to_f
+  end
 end
