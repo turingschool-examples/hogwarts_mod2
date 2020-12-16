0b2434596877d96aa008c3787da6ebbda56152a2
Sam Yeo <yeosamuel95@gmail.com>
2020-12-16 14:01:46 -0700

    Add instance method test for student full info

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  describe 'instance methods' do
+    before :each do
+      @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      @hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      @dumbdoor = Professor.create(name: "Dumble Door", age: 2 , specialty: "Unknown")
+  
+      @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+
+      ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
+      ProfessorStudent.create(student_id: @harry.id, professor_id: @hagarid.id)
+      ProfessorStudent.create(student_id: @harry.id, professor_id: @dumbdoor.id)
+    end
+    it "can list the student's name and how many professors they have" do
+      harry_content = "Harry Potter: 3"
+      expect(@harry.info).to eq(harry_content)
+    end
+  end
+
 end
