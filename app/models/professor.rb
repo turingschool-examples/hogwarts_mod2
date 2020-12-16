db865efb7333a3b8faed5ba91a3e8d7048fce805
Grayson Myers <graymyers10@gmail.com>
2020-12-16 13:26:46 -0700

    Add associations and validations

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,3 +1,7 @@
 class Professor <ApplicationRecord
-
+  has_many :professor_students
+  has_many :students, through: :professor_students
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :specialty, presence: true
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,7 @@
 class Student <ApplicationRecord
-
+  has_many :professor_students
+  has_many :professors, through: :professor_students
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :house, presence: true
 end
