77158fb10e44e644528f76baa149422e1bf36729
Grayson Cunningham <gecun289@gmail.com>
2020-12-16 13:48:31 -0700

    add tests for students index page

---
+++ spec/features/students/index_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Students Index" do
+  before :each do
+    snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+    lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+    harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+    longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+    @students = Student.all
+
+    ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+    ProfessorStudent.create(student_id: harry.id, professor_id: hagarid.id)
+    ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+    ProfessorStudent.create(student_id: malfoy.id, professor_id: hagarid.id)
+    ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+    ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+  end
+
+  it 'has a list of students' do
+    visit students_path
+
+    @students.each do |student|
+      expect(page).to have_css("#student-#{student.id}")
+    end
+  end
+
+  it 'shows the number of professors each student has' do
+    visit students_path
+
+    @students.each do |student|
+      within("#student-#{student.id}") do
+        expect(page).to have_content("#{student.name}: #{student.professor_count}")
+      end
+    end
+  end
+end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-
   describe 'validations' do
     it {should validate_presence_of :name}
     it {should validate_presence_of :age}
@@ -13,4 +12,21 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  describe 'instance methods' do
+    before :each do
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+
+      ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: hagarid.id)
+      ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+    end
+
+    it 'counts the number of professors a student has' do
+      expect(@harry.professor_count).to eq(3)
+    end
+  end
 end
