e5ed4a244b7983f378317b23984f695782fdbe7f
Catherine Dean <catherinemdean15@gmail.com>
2020-12-16 13:39:52 -0700

    create features folder

---
+++ spec/features/professors/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'Professors index page' do
+  before :each do
+    @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    @hagrid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+    @lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+  end
+
+  it "displays each professor in the system with attributes" do
+    visit '/professors'
+
+    expect(page).to have_content(@snape.name)
+    expect(page).to have_content(@snape.age)
+    expect(page).to have_content(@snape.specialty)
+    expect(page).to have_content(@hagrid.name)
+    expect(page).to have_content(@hagrid.age)
+    expect(page).to have_content(@hagrid.specialty)
+    expect(page).to have_content(@lupin.name)
+    expect(page).to have_content(@lupin.age)
+    expect(page).to have_content(@lupin.specialty)
+
+  end
+end
