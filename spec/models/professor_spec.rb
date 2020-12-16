c42a2f6ab1c05482bbb2ef78840d965e139aa625
Phil McCarthy <philmcc2631@gmail.com>
2020-12-16 14:04:31 -0700

    Add model test for students_avg_age instance method and order_by_name scope

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,39 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+  describe 'class methods' do
+    describe '::order_by_name (scope)' do
+      it 'returns all professors ordered alpha by name' do
+        snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+        hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+        lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+        expect(Professor.order_by_name).to eq([lupin, hagarid, snape])
+      end
+    end
+  end
+
+  describe 'instance methods' do
+    describe '#students_avg_age' do
+      it 'returns avg age of a professors students' do
+        snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+        hagarid = Professor.create(name: "Rubeus Hagrid", age: 38 , specialty: "Care of Magical Creatures")
+        lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+        harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+        malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+        longbottom = Student.create(name: "Neville Longbottom" , age: 11 , house: "Gryffindor" )
+
+        ProfessorStudent.create(student_id: harry.id, professor_id: snape.id)
+        ProfessorStudent.create(student_id: harry.id, professor_id: hagarid.id)
+        ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+        ProfessorStudent.create(student_id: malfoy.id, professor_id: hagarid.id)
+        ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+        ProfessorStudent.create(student_id: longbottom.id, professor_id: snape.id)
+
+        expect(snape.students_avg_age).to eq(11)
+        expect(hagarid.students_avg_age).to eq(11.5)
+        expect(lupin.students_avg_age).to eq(11.5)
+      end
+    end
+  end
 end
