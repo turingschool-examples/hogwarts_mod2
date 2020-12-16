20cf98326af956de6d19bc86e517cbd83e62122c
Max Ribbans <ribbans.max@gmail.com>
2020-12-16 15:08:20 -0600

    Add model test for student.num_professors

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student <ApplicationRecord
   has_many :professor_students
   has_many :professors, through: :professor_students
   validates :name, :age, :house, presence: true
+
+  def num_professors
+    professors.count
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe Student, type: :model do
     it {should have_many(:professors).through(:professor_students)}
   end
 
+  it "Can calculate the number of professors" do
+    @snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+    @harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+    ProfessorStudent.create(student_id: @harry.id, professor_id: @snape.id)
+    
+    expect(@harry.num_professors).to eq(1)
+  end
 end
